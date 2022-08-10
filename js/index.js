843d7fafc5e5496325aa78a62de1c7a6f8dd861e
nathanlaloux <nathan.laloux@cqumail.com>
2022-08-10 15:34:18 +1000

    Javascript

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// $(document).ready(function () {
+//     $(".apiCallButton").click(function (e) {
+
+//     });
+// });
+
+function fetchData(){
+    var parent = document.getElementById('test');
+    parent.innerHTML = "calling api...";
+    
+    $.ajax({
+        url: "https://wilmanagementsystem.azurewebsites.net/api/v1/student",
+        type: 'GET',
+        dataType: 'json', // added data type
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function(res) {
+            processApiData(res);
+            alert(JSON.stringify(res));
+        }
+        // put in error handling
+    });
+    
+    // fetch('https://wilmanagementsystem.azurewebsites.net/api/v1/student')
+    //     .then(response => response.json())
+    //     .then(response => processApiData(response))
+    //     .catch(err => console.error(err));
+}
+
+function processApiData(response){
+    var parent = document.getElementById('test');
+    parent.innerHTML = response;
+}
+
