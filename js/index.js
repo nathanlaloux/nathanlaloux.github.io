d5a30a2713c47e31570e42c378fc161ab7f8a804
nathanlaloux <nathan.laloux@cqumail.com>
2022-08-19 13:48:20 +1000

    Form added

--- js/index.js
+++ js/index.js
@@ -34,3 +34,35 @@ function processApiData(response){
     parent.innerHTML = response;
 }
 
+function createStudentAccount(){
+    alert("Submitting");
+    var student = {};
+    student.id = document.getElementById("studentId").value;
+    student.name = document.getElementById("studentName").value;
+    student.dob = document.getElementById("studentDob").value;
+    student.email = document.getElementById("studentEmail").value;
+
+    addStudent(student);
+    // document.getElementById('studentCreationForm').submit();
+}
+
+function addStudent(s){
+    $.ajax({
+        url: "https://wilmanagementsystem.azurewebsites.net/api/v1/student/add",
+        type: 'POST',
+        body: JSON.stringify(s),
+        dataType: 'json', // added data type
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function(res) {
+            alert(JSON.stringify(res));
+        },
+        error: function(res){
+            alert('error: ' + JSON.stringify(res));
+        }
+        // put in error handling
+    });
+}
+
